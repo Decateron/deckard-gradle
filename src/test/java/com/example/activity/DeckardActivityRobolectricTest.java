73e601d41c8564c936020d14e11a51c3b12df076
Josh <joshiheinrichs@gmail.com>
2015-01-16 14:35:27 -0600

    Removed unused imports

--- src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
+++ src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
@@ -1,12 +1,8 @@
 package com.example.activity;
 
 import android.app.Activity;
-import com.example.robolectric.DeckardActivity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertTrue;
 
